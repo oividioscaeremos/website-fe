55758425b2297b3f609432feaba7e59642820ea7
İlker Atabay <atabayilker@gmail.com>
2022-08-02 01:33:11 +0300

    Adding the rest.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { ArticleDetailComponent } from './components/public/blog/article-detail/article-detail.component';
+import { BlogComponent } from './components/public/blog/blog.component';
+import { ContactComponent } from './components/public/contact/contact.component';
+import { AboutMeComponent } from './components/public/about-me/about-me.component';
+import { PublicComponent } from './components/public/public.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      { path: 'about-me', component: AboutMeComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'blog', component: BlogComponent },
+      { path: 'blog/details', component: ArticleDetailComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PublicComponent } from './components/public/public.component';
+import { LandingPageComponent } from './components/public/landing-page/landing-page.component';
+import { AboutMeComponent } from './components/public/about-me/about-me.component';
+import { ContactComponent } from './components/public/contact/contact.component';
+import { BlogComponent } from './components/public/blog/blog.component';
+import { HeaderComponent } from './components/public/header/header.component';
+import { FooterComponent } from './components/public/footer/footer.component';
+import { ArticleComponent } from './components/public/blog/article/article.component';
+import { ArticleDetailComponent } from './components/public/blog/article-detail/article-detail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    LandingPageComponent,
+    AboutMeComponent,
+    ContactComponent,
+    BlogComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticleComponent,
+    ArticleDetailComponent
   ],
   imports: [
     BrowserModule,
